5b8eb0ea5ddcf93a4bd4767c7941870a5b98432b
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2023-01-25 20:54:12 -0500

    Continue with image process...

--- processing_image.py
+++ processing_image.py
@@ -16,48 +16,79 @@ Colores conforme a los numeros en cada matriz:
 """
 ######################################################################################
 
-from PIL import Image
+# Try with numpy, pillow
 
-"""
+from PIL import Image, ImageFilter
+import numpy as np
 
-"""
-def discretize_image(file_image : str, rescaled_height = 30, rescaled_width = 30):
+class SquarePixel():
+    def __init__(self, limit_min_x, limit_min_y, limit_max_x, limit_max_y) -> None:
+        self.colors = []        
+        self.limit_min_x = limit_min_x
+        self.limit_min_y = limit_min_y
+        self.limit_max_x = limit_max_x
+        self.limit_max_y = limit_max_y        
+
+    def add_color(self, color: tuple, x: int, y: int):
+        if(x >= self.limit_min_x and x <= self.limit_max_x and y >= self.limit_min_y and y <= self.limit_max_y):
+            self.colors.append(color)
+
+    def see_all_colors(self):
+        for j in self.colors:
+            print(j)
+
+    def get_color(self) -> tuple:
+        sum_r = 0
+        sum_g = 0
+        sum_b = 0
+        for colors in self.colors:
+            r, g, b = colors        
+            sum_r += r
+            sum_g += g
+            sum_b += b
+        elements = len(self.colors)
+
+        color_mean = (int(sum_r / elements), int(sum_g / elements), int(sum_b / elements))
+
+        # Vamos a determinar el color definitivo
+        
+        return 
+
+def discretize_image(file_image : str, pixels_square_h = 20, pixels_square_w = 20):
     img = Image.open(file_image)
-    img.thumbnail((rescaled_height, rescaled_width))
-    
+
+    # Tamanio
     width = img.size[0]
     height = img.size[1]
-
-    matrix = []
-    for row in range(height):
-        support_row = []
-        for col in range(width):
+    
+    # Cantidad de cuadros a dibujar
+    amount_square_x = int(width / pixels_square_w)
+    amount_square_y = int(height / pixels_square_h)
+    
+    squares = [] # Almacenar todos los cuadros con sus limites correspondientes
+    # Definir limites para cada cuadro de pixeles
+    aux_x1 = 0
+    aux_y1 = pixels_square_h - 1
+    aux_x2 = 0
+    aux_y2 = pixels_square_h - 1
+    for x in range(amount_square_x):
+        for y in range(amount_square_y):
+            square_of_pixel = SquarePixel(aux_x1, aux_y1, aux_x2, aux_y2)            
+            squares.append(square_of_pixel)
+            aux_x2 += pixels_square_w
+            aux_y2 += pixels_square_h        
+        aux_x1 += pixels_square_w
+        aux_y1 += pixels_square_h
+        aux_x2 = 0
+        aux_y2 = pixels_square_h - 1
+    
+    # Definir todos los pixeles en los cuadros
+    for row in range(width):
+        for col in range(height):
             colors = img.getpixel((row, col))
-            matrix.append(colors)
-        matrix.append(support_row)        
-    return
+            for s in squares:
+                s.add_color(colors, row, col)
 
-def test_matrix(): # Para probar por si acaso :)
-    matrix = [
-        ['#','#','#',' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-        ['#','#','#',' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-        ['#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#','#',' ','g',' ',' ',' '],
-        ['#','#','#','#','#',' ',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ',' ',' ',' '],
-        ['#','#','#','#','#','#',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ','#','#','#'],
-        ['#',' ',' ','#','#','#',' ',' ',' ',' ',' ','#','#','#','#',' ',' ','#','#','#'],
-        ['#',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ','#','#','#',' ',' ',' ',' ',' '],
-        [' ',' ','g',' ','#','#',' ',' ','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' '],
-        ['#','#',' ',' ','#',' ',' ',' ','#','#','#','#',' ',' ',' ',' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ','#','#','#','#',' ',' '],
-        ['#','#',' ',' ','#',' ','#','#','#',' ',' ','#',' ','#','#','#','#','#',' ',' '],
-        ['#','#',' ',' ',' ',' ','#','#','#',' ',' ',' ',' ','#','#','#','#','#','#',' '],
-        ['#','#','#','#','#',' ',' ','#','#',' ',' ',' ',' ','#','#','#','#','#','#','#'],
-        [' ',' ','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#','#','#'],
-        [' ',' ','#','#','#','#','#','#','#',' ',' ','#','#',' ',' ',' ','#','#','#','#'],
-        [' ',' ','#','#','#','#','#','#','#',' ','#','#','#',' ',' ',' ','#','#','#','#'],
-        ['#','#','#',' ',' ','#','#','#','#',' ',' ',' ',' ',' ',' ','r','#','#','#','#'],
-        ['#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#','#','#'],
-        ['#','#','#',' ',' ',' ',' ',' ',' ','#','#','#','#',' ',' ',' ','#','#','#','#'],
-        ['#','#',' ',' ',' ',' ',' ',' ',' ','#','#','#','#',' ',' ',' ','#','#','#','#'],
-    ]
-    return matrix
+    
+
+    return
