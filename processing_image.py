bbe4f6dde1c3fcafe64c29d6b90c32cd07909f4d
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2023-01-27 14:05:49 -0800

    Refactoring Image Processing

--- processing_image.py
+++ processing_image.py
@@ -9,10 +9,10 @@ Processing Image
     en los laberintos.
 
 Colores conforme a los numeros en cada matriz:
-    - ' ': Cuadro libre (Cuadro Blanco)
-    - '#': Cuadro ocupado (Cuadro Negro)
-    - 'r': Punto de inicio (Cuadro Rojo)
-    - 'g': Punto de victorio (Cuadro Verde)
+    - '0': Cuadro libre (Cuadro Blanco)
+    - '1': Cuadro ocupado (Cuadro Negro)
+    - '2': Punto de inicio (Cuadro Rojo)
+    - '3': Punto de victorio (Cuadro Verde)
 """
 ######################################################################################
 
@@ -21,104 +21,78 @@ Colores conforme a los numeros en cada matriz:
 from PIL import Image, ImageFilter
 import numpy as np
 
-class SquarePixel():
-    def __init__(self, limit_min_x, limit_min_y, limit_max_x, limit_max_y) -> None:
-        self.colors = []        
-        self.limit_min_x = limit_min_x
-        self.limit_min_y = limit_min_y
-        self.limit_max_x = limit_max_x
-        self.limit_max_y = limit_max_y        
-
-    def add_color(self, color: tuple, x: int, y: int):
-        if(x >= self.limit_min_x and x <= self.limit_max_x and y >= self.limit_min_y and y <= self.limit_max_y):
-            self.colors.append(color)
-
-    def see_all_colors(self):
-        for j in self.colors:
-            print(j)
-
-    def get_color(self) -> tuple:
-        sum_r = 0
-        sum_g = 0
-        sum_b = 0
-        for colors in self.colors:
-            r, g, b = colors        
-            sum_r += r
-            sum_g += g
-            sum_b += b
-        elements = len(self.colors)
-
-        color_mean = (int(sum_r / elements), int(sum_g / elements), int(sum_b / elements))
-
-        # Vamos a determinar el color definitivo
-        r = color_mean[0]
-        g = color_mean[1]
-        b = color_mean[2]
-
-        if(r >= 200 and g >= 200 and b >= 200): # Blanco
-            return 0 
-
-        if(r <= 50 and g <= 50 and b <= 50): # Negro
-            return 1
-        
-        if(r >= 200 and g <= 50 and b <= 50): # Rojo (Punto de inicio)
-            return 3
-
-        if(r <= 50 and g >= 200 and b <= 50): # Verde (Puntos de meta)
-            return 4
-
-        return -1
-
 def discretize_image(file_image : str, pixels_square_h = 20, pixels_square_w = 20):
     img = Image.open(file_image)
-    img.thumbnail((100, 100))
-    img.show()
+    pixels = img.load()
 
     # Tamanio
     width = img.size[0]
     height = img.size[1]
-    
-    # Cantidad de cuadros a dibujar
-    amount_square_x = int(width / pixels_square_w)
-    amount_square_y = int(height / pixels_square_h)
-    
-    squares = [] # Almacenar todos los cuadros con sus limites correspondientes
-    # Definir limites para cada cuadro de pixeles
-    aux_x1 = 0
-    aux_y1 = pixels_square_h - 1
-    aux_x2 = 0
-    aux_y2 = pixels_square_h - 1
-    for x in range(amount_square_x):
-        for y in range(amount_square_y):
-            square_of_pixel = SquarePixel(aux_x1, aux_y1, aux_x2, aux_y2)            
-            squares.append(square_of_pixel)
-            aux_x2 += pixels_square_w
-            aux_y2 += pixels_square_h        
-        aux_x1 += pixels_square_w
-        aux_y1 += pixels_square_h
-        aux_x2 = 0
-        aux_y2 = pixels_square_h - 1
-    
-    # Definir todos los pixeles en los cuadros
-    for row in range(width):
-        for col in range(height):
-            colors = img.getpixel((row, col))
-            for s in squares:
-                s.add_color(colors, row, col)
-
-    # Obtener el promedio y la matriz de todos los cuadros creados    
+                        
+    # Vamos a llevar a cabo la suma de los pixeles
     matrix = []
-    count = 1
-    matrix_aux = []
-    for s in squares:
-        if count >= amount_square_x:
-            matrix.append(matrix_aux)
-            count = 0
-            matrix_aux = []
-        else:
-            matrix_aux.append(s.get_color())
-        count += 1
-
+    for x in range(width):
+        matrix_column = []
+        aux_pixels = []
+        for y in range(height):
+            pixel = pixels[x, y]
+            # Se llevara a cabo la suma y promedios de los colores para luego guardarlo en la matriz
+            if(len(aux_pixels) >= pixels_square_h):
+                pixel_mean = get_color_mean(aux_pixels)
+                matrix_column.append(pixel_mean)
+                aux_pixels = []
+            # Se guardara todos los pixeles en un listado auxiliar
+            else:                
+                aux_pixels.append(pixel)
+        matrix.append(matrix_column)
+        
+    # Revertir valores para obtener el promedio de los colores pendientes
+    matrix_zip = zip(*matrix)
+    matrix_zip_list = list(matrix_zip)
+        
+    # Vamos a obtener los promedios pendientes
+    pixels = matrix_zip_list
+    matrix = []
+    for x in range(len(matrix_zip_list)):
+        matrix_column = []
+        aux_pixels = []
+        for y in range(len(matrix_zip_list[0])):
+            pixel = pixels[x][y]
+            if(len(aux_pixels) >= pixels_square_h):
+                pixel_mean = get_color_mean(aux_pixels)
+                matrix_column.append(pixel_mean)
+                aux_pixels = []
+            # Se guardara todos los pixeles en un listado auxiliar
+            else:                
+                aux_pixels.append(pixel)
+        matrix.append(matrix_column)
+        
     print(matrix)
-
+    print(len(matrix))
+    print(len(matrix[0]))
     return 
+
+def get_color_mean(pixels : list):
+    sum_r = 0
+    sum_g = 0
+    sum_b = 0
+    
+    for colors in pixels:
+        r, g, b = colors
+        sum_r += r
+        sum_g += g
+        sum_b += b
+        
+    elements = len(pixels)        
+    return (int(sum_r / elements), int(sum_g / elements), int(sum_b / elements))
+
+def create_img_solution(matrix : list, w : int, h : int):
+    data = np.zeros((h, w, 3), dtype=np.uint8)
+    
+    # Vamos a procesar toda la data para crear la imagen
+    
+    # Crear la imagen
+    img = Image.fromarray(data, 'RGB')
+    img.show()
+    return
+    
